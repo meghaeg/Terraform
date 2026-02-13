ba279eb34f10055aba474ebe3baf265a53990f17
meghaeg <155151663+meghaeg@users.noreply.github.com>
2026-02-13 12:09:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_instance" "dev_server" {
-  count=5
+
+  count = length(data.aws_availability_zones.available.names)
+
   ami           = var.ami_id
   instance_type = var.instance_type
+
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   user_data = file("userdata.sh")
 
   tags = {
-    Name = "server"
+    Name = "Terraform-${data.aws_availability_zones.available.names[count.index]}-Server-${count.index + 1}"
   }
 }
-
-
