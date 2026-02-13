9bd2b97605a9d17209b9cd7def664fd21cc80396
meghaeg <155151663+meghaeg@users.noreply.github.com>
2026-02-13 14:27:45 +0530

    Refactor userdata.sh by cleaning comments and updating Java
    Removed unnecessary comments and updated Java installation step.

--- userdata.sh
+++ userdata.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# Update system
 sudo dnf update -y
 
-# --------------------
 # Install Git
-# --------------------
 sudo dnf install git -y
 
-# --------------------
 # Install Docker
-# --------------------
 sudo dnf install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 
-# --------------------
-# Install Java (Jenkins requirement)
-# --------------------
+# Install Java
 sudo dnf install java-17-amazon-corretto -y
 
-# --------------------
 # Install Jenkins
-# --------------------
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
