4495d7805b91cd406901691e0d2e191dc5a87e2e
meghaeg <155151663+meghaeg@users.noreply.github.com>
2026-02-13 14:15:15 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y nginx
-sudo systemctl start nginx
+
+# Update system
+sudo apt update -y
+
+# ----------------------------
+# Install Git
+# ----------------------------
+sudo apt install git -y
+
+# ----------------------------
+# Install Docker
+# ----------------------------
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+
+# ----------------------------
+# Install Java (Required for Jenkins)
+# ----------------------------
+sudo apt install openjdk-17-jdk -y
+
+# ----------------------------
+# Install Jenkins
+# ----------------------------
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+sudo apt install jenkins -y
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
