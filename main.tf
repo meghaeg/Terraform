d420bf6d59d9660402c4b2e933968e7a28196abd
meghaeg <155151663+meghaeg@users.noreply.github.com>
2026-02-13 14:15:34 +0530

    Add security group and modify dev_server instance

--- main.tf
+++ main.tf
@@ -1,19 +1,39 @@
-data "aws_availability_zones" "available" {
-  state = "available"
+resource "aws_security_group" "devops_sg" {
+
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "dev_server" {
 
-  count = length(data.aws_availability_zones.available.names)
-
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
   user_data = file("userdata.sh")
 
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
   tags = {
-    Name = "Terraform-${data.aws_availability_zones.available.names[count.index]}-Server-${count.index + 1}"
+    Name = "DevOps-Server"
   }
 }
